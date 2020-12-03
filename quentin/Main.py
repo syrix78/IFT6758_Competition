799ddb009fba2ec49a3a629ab9bb058801346bb1
Quentin WOLAK <quentinwlk68@gmail.com>
2020-12-04 00:18:39 +0100

    Okay test sur pycharm

--- quentin/Main.py
+++ quentin/Main.py
@@ -2,10 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# In[15]:
+# In[1]:
 if __name__ == "__main__":
     print("test")
     #df_train = pd.read_csv("/Users/quentinwolak/Desktop/Cours/UdeM/quatrieme_annee/IFT6758 /Kaggle/ift6758-a20.nosync/data/train.csv")
 
-    
-# %%
+# In[2]:
+print("ok")
